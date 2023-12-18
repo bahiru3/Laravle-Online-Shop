e11b89d9bfcad58733c89b1d9457f5ad58ccbd03
bahiru3 <bahirumegersa681@gmail.com>
2023-12-18 15:28:34 +0300

    e

---
+++ BootStrap/js/index.php
@@ -0,0 +1,22 @@
+<?php
+ function funcProf()
+ {
+     echo "This is Professionals for Professionals";
+ }
+ // function along with three parameters
+function proProf($num1, $num2, $num3,$num4)
+{
+    $product = $num1 * $num2 * $num3*$num4;
+    echo "The product is $product";
+}
+ 
+// Calling the function
+// Passing three arguments
+proProf(2, 3, 5,2);
+
+ 
+ // Calling the function
+ funcProf();
+ 
+  
+?>
