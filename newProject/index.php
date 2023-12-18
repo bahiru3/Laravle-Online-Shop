3d859c6d71f73691ec6521034c1b9bd06b40341f
bahiru3 <bahirumegersa681@gmail.com>
2023-12-18 15:36:35 +0300

    po

--- newProject/index.php
+++ newProject/index.php
@@ -1,4 +1,4 @@
-<form>
+<!-- <form>
  <input type="text" name="name" />
  <input type="submit" />
 </form>
@@ -8,4 +8,16 @@
 if (isset($_GET['name'])) {
  echo '<p>The name is ' . $_GET['name'];
 }
+?> -->
+<form method="POST">
+ <input type="text" name="name" />
+ <input type="submit" />
+</form>
+
+
+<?php
+if (isset($_POST['name'])) {
+ echo '<p>The name is ' . $_POST['name'];
+}
 ?>
+
